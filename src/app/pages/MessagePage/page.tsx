295625e89924881fd77a6b43401d9900e8e89136
MarcosRodriguez03 <marcos.e.rodriguez2003@gmail.com>
2024-03-21 14:14:33 -0700

    branch

---
+++ src/app/pages/MessagePage/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MessagePage = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default MessagePage
---
+++ src/interface/interface.ts
@@ -0,0 +1,10 @@
+export interface iUserInfo {
+    username: string
+    email: string
+    password: string
+    id: number
+    firstName: string
+    lastName: string
+    bio: string
+    profileImage: string
+}
